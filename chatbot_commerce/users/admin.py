9a809639ac38d0cbd276b43d4543d7524f02d540
Yesid <yesidf26@gmail.com>
2021-06-07 10:33:16 -0500

    project init

---
+++ chatbot_commerce/users/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.contrib.auth import admin as auth_admin
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
+
+# from chatbot_commerce.users.forms import UserChangeForm, UserCreationForm
+
+
