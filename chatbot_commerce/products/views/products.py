b023a8a81d1cc096d3191f4a378e9fc91fd23cb5
Yesid <yesidf26@gmail.com>
2021-07-26 00:03:44 -0500

    post method to get products

--- chatbot_commerce/products/views/products.py
+++ chatbot_commerce/products/views/products.py
@@ -1,8 +1,10 @@
 """Products and skus views."""
 
 # Django Rest Framewor
-from django.db.models.fields import SlugField
+from django.http import Http404
 from rest_framework import viewsets
+from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
+from rest_framework.response import Response
 from rest_framework.decorators import action
 
 # Serializers
@@ -17,9 +19,29 @@ class ProductsViewset(viewsets.ModelViewSet):
 
     serializer_class = ProductsModelSerializer
     lookup_field = 'product_id'
+    allowed_methods = ['get', 'post']
 
-    @action(detail=True, methods=['get', 'post'])
-    def get_queryset(self):
+    def get_queryset(self, *args, **kwargs):
         """Restrict list to products active."""
-        queryset = ProductsApiVtex.objects.filter(is_active=True)
-        return queryset
+        self.queryset = ProductsApiVtex.objects.filter(is_active=True)
+        return self.queryset
+
+    def create(self, request, *args, **kwargs):
+        try:
+            self.get_object()
+        except (Http404, AssertionError):
+            data = self.serializer_class(self.queryset, many=True).data
+            status = HTTP_200_OK
+        return Response(data=data, status=status)
+
+    @action(methods=['post'], detail=True)
+    def post(self, *args, **kwargs):
+        """Restrict list to products active."""
+        obj = self.get_object()
+        data = self.serializer_class(obj).data
+        status = HTTP_200_OK
+        return Response(data=data, status=status)
+    
+
+
+    
