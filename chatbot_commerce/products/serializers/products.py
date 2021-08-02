0df04e2fb428ed129395e929dea8b542313854e2
EverUnLOver <anewmeal@gmail.com>
2021-08-01 22:48:36 -0500

    implement/pagination-urlfilter-products part3

--- chatbot_commerce/products/serializers/products.py
+++ chatbot_commerce/products/serializers/products.py
@@ -1,12 +1,8 @@
 """Products serializers."""
 
-# Django
-from django.db.models.signals import post_init, pre_init
-from django.dispatch import receiver
-
 # Django rest framework
 from chatbot_commerce.products.models.skus import FixedPrice, Price, Skus, Image, DateRange
-from rest_framework import fields, serializers
+from rest_framework import serializers
 
 # Model
 from chatbot_commerce.products.models import ProductsApiVtex
