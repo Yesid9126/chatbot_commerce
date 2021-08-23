f32c61a7cdb4d2dadcc39db9a7cb25cdb569a6e5
Yesid9126 <yesidf26@gmail.com>
2021-08-23 13:53:04 -0500

    order model

--- chatbot_commerce/webhooks/admin.py
+++ chatbot_commerce/webhooks/admin.py
@@ -1,3 +1,24 @@
+"""Webhooks models admin."""
+
+# Django
 from django.contrib import admin
 
-# Register your models here.
+# Models
+from chatbot_commerce.webhooks.models import Order, OrderItem
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    """Order model admin."""
+
+    list_display = ['id', 'customer', 'status']
+    search_fields = ['id', 'customer']
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    """Order item admin."""
+
+    list_display = ['order', 'quantity']
+    search_fields = ['order__customer',]
+
--- chatbot_commerce/webhooks/models/webhooks.py
+++ chatbot_commerce/webhooks/models/webhooks.py
@@ -1,4 +1,85 @@
-"""Webhook model."""
+"""Webhook models."""
 
 # Django
 from django.db import models
+from django.contrib.postgres.fields import JSONField
+
+# utilities
+from chatbot_commerce.utils.models import ChatbootModel
+
+class Order(ChatbootModel):
+    """Order class."""
+
+    DONE = 'DONE'
+    PENDING = 'PENDING'
+    FAILED = 'FAILED'
+
+    STATUS_CHOICES = [
+        (DONE, DONE),
+        (FAILED, FAILED),
+        (PENDING, PENDING),
+    ]
+    status = models.CharField(
+        'Status',
+        max_length=255,
+        choices=STATUS_CHOICES,
+        default=PENDING
+    )
+
+    customer = models.CharField(
+        max_length=100,
+    )
+
+    price = models.PositiveIntegerField(
+        default=0,
+    )
+
+    hook_data = models.JSONField('Hook complete data', blank=True, null=True)
+
+    def __str__(self):
+        """Return order id."""
+        return str(self.customer)
+
+    class Meta:
+        """Meta class."""
+
+        verbose_name = "Order"
+        verbose_name_plural = "Orders"
+
+
+class OrderItem(ChatbootModel):
+    """Order item."""
+
+    sku_unit = models.OneToOneField(
+        to='products.Skus',
+        on_delete=models.CASCADE,
+        related_name='sku'
+    )
+
+    order = models.ForeignKey(
+        to='Order',
+        on_delete=models.CASCADE,
+        related_name='item',
+    )
+
+    quantity = models.CharField(
+        max_length=10,
+        blank=True,
+        null=True
+    )
+
+    price = models.CharField(
+        max_length=10,
+        blank=True,
+        null=True
+    )
+
+    def __str__(self):
+        """Return order id."""
+        return str(self.sku_unit)
+
+    class Meta:
+        """Meta class."""
+
+        verbose_name = "Order Item"
+        verbose_name_plural = "Order Items"
