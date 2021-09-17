a01434396f2ef759e926ad6f406183b2712d5b6b
EverUnLOver <anewmeal@gmail.com>
2021-09-16 23:00:59 -0500

    no matters 2

--- config/urls.py
+++ config/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    #path('prueba/', view=product_list),
+    # path('prueba/', view=product_list),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
