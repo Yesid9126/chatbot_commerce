a7b29e0d118a85a73d97998fc1fb26e075ad6466
Santiago Vallejo <santiagovallejoc@gmail.com>
2021-09-21 13:07:47 -0500

    fix qa.py qa

--- config/settings/qa.py
+++ config/settings/qa.py
@@ -97,11 +97,11 @@ aws_s3_domain = AWS_S3_CUSTOM_DOMAIN or f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws
 # ------------------------
 STATICFILES_STORAGE = "chatbot_commerce.utils.storages.StaticRootS3Boto3Storage"
 COLLECTFAST_STRATEGY = "collectfast.strategies.boto3.Boto3Strategy"
-STATIC_URL = f"https://{aws_s3_domain}/static/"
+STATIC_URL = f"https://{aws_s3_domain}/qa/static/"
 # MEDIA
 # ------------------------------------------------------------------------------
 DEFAULT_FILE_STORAGE = "chatbot_commerce.utils.storages.MediaRootS3Boto3Storage"
-MEDIA_URL = 'https://{}/'.format(AWS_S3_CUSTOM_DOMAIN)
+MEDIA_URL = 'https://{}/qa'.format(AWS_S3_CUSTOM_DOMAIN)
 
 # TEMPLATES
 # ------------------------------------------------------------------------------
