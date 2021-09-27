0839103f0023ba9ae2d0c6c4b9c5a3a37fea67a0
EverUnLOver <anewmeal@gmail.com>
2021-09-27 00:25:42 -0500

    fool error

--- chatbot_commerce/products/filters/products.py
+++ chatbot_commerce/products/filters/products.py
@@ -10,7 +10,7 @@ from django.db.models import Q, Prefetch
 
 class ProductFilterSet(FilterSet):
     skus__attributes__attribute_type__name__icontains = filters.CharFilter(
-        field_name='sku_name',
+        field_name='skus__attributes__attribute_type__name',
         lookup_expr='icontains',
         label='Type of attribute',
     )
@@ -27,7 +27,7 @@ class ProductFilterSet(FilterSet):
 
     class Meta:
         model = Product
-        fields = ('skus__sku_name__icontains', 'skus__attributes__attribute_type__name__icontains', 'skus__attributes__value__icontains')
+        fields = ('skus__name__icontains', 'skus__attributes__attribute_type__name__icontains', 'skus__attributes__value__icontains')
 
 
 def products_skus(self):
