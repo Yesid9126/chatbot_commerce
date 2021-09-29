3108d91fd3380c02b99cffbb22cf756183544cf5
EverUnLOver <anewmeal@gmail.com>
2021-09-29 00:02:06 -0500

    search filters

--- chatbot_commerce/products/filters/products.py
+++ chatbot_commerce/products/filters/products.py
@@ -254,7 +254,7 @@ def filter_data_skus(self):
         skus_filter_data |= {'total_quantity': total_quantity}
     search = self.data.get('search')
     if search:
-        skus_filter_data |= {'search__icontains': search}
+        skus_filter_data |= {'search__search': search}
     attributes = self.data.get('attributes')
     if attributes:
         attributes = attributes.replace('-', ' ')
