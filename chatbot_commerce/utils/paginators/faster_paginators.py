f85609e22e1588042dba7b38f6e203241a49ce19
EverUnLOver <anewmeal@gmail.com>
2021-10-07 15:38:07 -0500

    new performance

--- chatbot_commerce/utils/paginators/faster_paginators.py
+++ chatbot_commerce/utils/paginators/faster_paginators.py
@@ -1,7 +1,8 @@
 from django.core.paginator import Paginator
 from django.utils.functional import cached_property
 from rest_framework.pagination import PageNumberPagination
-
+from rest_framework.response import Response
+from collections import OrderedDict
 
 class FastDjangoPaginator(Paginator):
     @cached_property
@@ -11,3 +12,13 @@ class FastDjangoPaginator(Paginator):
 
 class FasterPagenumberPagination(PageNumberPagination):
     django_paginator_class = FastDjangoPaginator
+
+    def get_paginated_response(self, data):
+        return Response(
+            {
+                'count': self.page.paginator.count,
+                'next': self.get_next_link(),
+                'previous': self.get_previous_link(),
+                'results': data
+            }
+        )
