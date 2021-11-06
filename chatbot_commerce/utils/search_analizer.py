9dd2ba80b00a149b184541a508282551a5ca4f75
EverUnLOver <anewmeal@gmail.com>
2021-11-06 02:46:24 -0500

    add file search analizer for testing

---
+++ chatbot_commerce/utils/search_analizer.py
@@ -0,0 +1,62 @@
+# Models
+from stores.models import SearchAnalizer
+
+# Utils
+import time
+
+
+def search_analizer_manager(self):
+    obj, _ = SearchAnalizer.objects.get_or_create(store=self.store, search=self.search, defaults={'date_count': {'count': 0}})
+    year, month, day, hour = time.strftime('%Y-%m-%d-%H').split('-')
+    count = 'count'
+    try:
+        condition = obj.date_count[year][month][day].get(hour)
+        if condition:
+            obj.date_count[year][month][day][hour] += 1
+        else:
+            obj.date_count[year][month][day][hour] = 1
+        obj.date_count[year][month][day][count] += 1
+        obj.date_count[year][month][count] += 1
+        obj.date_count[year][count] += 1
+        obj.date_count[count] += 1
+    except (KeyError,) as reason:
+        reason = str(reason).replace("'", "")
+        if reason == year:
+            obj.date_count |= {
+                year: {
+                    month: {
+                        day: {
+                            hour: 1,
+                            count: 1
+                        },
+                        count: 1
+                    },
+                    count: 1
+                },
+            }
+            obj.date_count[count] += 1
+
+        elif reason == month:
+            print('noo')
+            obj.date_count[year] |= {
+                month: {
+                    day: {
+                        hour: 1,
+                        count: 1
+                    },
+                    count: 1
+                },
+            }
+            obj.date_count[count] += 1
+            obj.date_count[year][count] += 1
+
+        elif reason == day:
+            obj.date_count[year][month] |= {
+                day: {
+                    hour: 1,
+                    count: 1
+                },
+            }
+            obj.date_count[count] += 1
+            obj.date_count[year][count] += 1
+            obj.date_count[year][month][count] += 1
