5cd07da6ff769f518b5ca66b034a6464d75b1296
TodoViernes <santiagovallejoc@gmail.com>
2023-09-18 15:44:45 -0600

    whitenoise

--- config/settings/production.py
+++ config/settings/production.py
@@ -95,9 +95,9 @@ AWS_S3_CUSTOM_DOMAIN = 'chatbotapi.nyc3.digitaloceanspaces.com'
 aws_s3_domain = AWS_S3_CUSTOM_DOMAIN or f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 # STATIC
 # ------------------------
-STATICFILES_STORAGE = "chatbot_commerce.utils.storages.StaticRootS3Boto3Storage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 COLLECTFAST_STRATEGY = "collectfast.strategies.boto3.Boto3Strategy"
-STATIC_URL = f"https://{aws_s3_domain}/static/"
+STATIC_URL = f"/static/"
 # MEDIA
 # ------------------------------------------------------------------------------
 DEFAULT_FILE_STORAGE = "chatbot_commerce.utils.storages.MediaRootS3Boto3Storage"
